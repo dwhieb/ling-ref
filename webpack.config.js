da3171b318b7db1ecd5ec53ddbcc51e1519f4fed
Danny Hieber <dwhieb@gmail.com>
2019-04-05 22:56:42 -0700

    add scripts to test page

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require(`path`);
+const CopyPlugin = require(`copy-webpack-plugin`);
+const path       = require(`path`);
 
 module.exports = {
   entry:     path.join(__dirname, `src/index.js`),
@@ -17,4 +18,16 @@ module.exports = {
     libraryTarget: `umd`,
     path:          __dirname,
   },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, `LingRef.js`),
+        to:   path.join(__dirname, `test/LingRef.js`),
+      },
+      {
+        from: path.join(__dirname, `node_modules/handlebars/dist/handlebars.js`),
+        to:   path.join(__dirname, `test/handlebars.js`),
+      },
+    ]),
+  ],
 };
