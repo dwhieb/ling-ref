2130a43841a8e17e5ee021d312a14d094b8ee089
Danny Hieber <dwhieb@gmail.com>
2019-04-06 13:18:24 -0700

    option: template
    closes #82

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 const Handlebars       = require(`handlebars`);
 const helpers          = require(`./helpers`);
-const path             = require(`path`);
 const { readFileSync } = require(`fs`);
-
-const templatePath = path.join(__dirname, `reference.hbs`);
-
-const defaults = {
-  handlebars: Handlebars,
-};
 
 class LingRef {
 
-  constructor(options = defaults) {
+  constructor({
+    handlebars = Handlebars,
+    partial    = `reference`,
+    template   = `reference.hbs`,
+  } = {}) {
 
-    const { handlebars } = options;
-
-    this.handlebars = handlebars;
-    this.template   = readFileSync(templatePath, `utf8`);
-    this.compile    = this.handlebars.compile(this.template);
-    this.handlebars.registerPartial({ reference: this.template });
+    this.handlebars   = handlebars;
+    this.partialName  = partial;
+    this.templatePath = template;
+    this.template     = readFileSync(this.templatePath, `utf8`);
+    this.compile      = this.handlebars.compile(this.template);
+    this.handlebars.registerPartial({ [this.partialName]: this.template });
     this.handlebars.registerHelper(helpers);
 
   }
--- test/node.test.js
+++ test/node.test.js
@@ -1,27 +1,89 @@
-const Handlebars = require(`handlebars`);
-const LingRef    = require(`../src/index.js`);
+const Handlebars       = require(`handlebars`);
+const LingRef          = require(`../src/index.js`);
+const path             = require(`path`);
+const { readFileSync } = require(`fs`);
+
+const templatePath = path.join(__dirname, `../src/reference.hbs`);
 
 describe(`LingRef`, () => {
 
   it(`registers the partial with Handlebars`, () => {
-    new LingRef; // eslint-disable-line no-new
-    const compile = Handlebars.compile(`{{> reference}}`);
+
+    const hbs = Handlebars.create();
+
+    new LingRef({
+      handlebars: hbs,
+      template:   templatePath,
+    });
+
+    const compile = hbs.compile(`{{> reference}}`);
     const html    = compile({});
+
     expect(typeof html).toBe(`string`);
+
   });
 
   it(`.compile()`, () => {
-    const lingRef = new LingRef;
-    const html    = lingRef.compile({});
+
+    const hbs = Handlebars.create();
+
+    const lingRef = new LingRef({
+      handlebars: hbs,
+      template:   templatePath,
+    });
+
+    const html = lingRef.compile({});
+
     expect(typeof html).toBe(`string`);
+
   });
 
   describe(`options`, () => {
 
     it(`handlebars`, () => {
+
       const hbs = Handlebars.create();
-      new LingRef({ handlebars: hbs }); // eslint-disable-line no-new
+
+      new LingRef({
+        handlebars: hbs,
+        template:   templatePath,
+      });
+
       expect(hbs.partials.reference).toBeDefined();
+
+    });
+
+    it(`partial`, () => {
+
+      const hbs = Handlebars.create();
+
+      new LingRef({
+        handlebars: hbs,
+        partial:    `ref`,
+        template:   templatePath,
+      });
+
+      expect(hbs.partials.reference).toBeUndefined();
+      expect(hbs.partials.ref).toBeDefined();
+
+    });
+
+    it(`template`, () => {
+
+      const hbs = Handlebars.create();
+
+      // eslint-disable-next-line no-shadow
+      const templatePath = path.join(__dirname, `jasmine.json`);
+
+      new LingRef({
+        handlebars: hbs,
+        template:   templatePath,
+      });
+
+      const template = readFileSync(templatePath, `utf8`);
+
+      expect(hbs.partials.reference).toBe(template);
+
     });
 
   });
