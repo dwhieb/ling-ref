53033aec468f3618020770a74041e25cd62175cc
Danny Hieber <dwhieb@gmail.com>
2019-04-06 12:43:14 -0700

    add handlebars option
    closes #77

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-const handlebars       = require(`handlebars`);
+const Handlebars       = require(`handlebars`);
 const helpers          = require(`./helpers`);
 const path             = require(`path`);
 const { readFileSync } = require(`fs`);
 
 const templatePath = path.join(__dirname, `reference.hbs`);
 
-const defaults = {};
+const defaults = {
+  handlebars: Handlebars,
+};
 
 class LingRef {
 
   constructor(options = defaults) {
-    this.template = readFileSync(templatePath, `utf8`);
-    this.compile  = handlebars.compile(this.template);
-    handlebars.registerPartial({ reference: this.template });
-    handlebars.registerHelper(helpers);
+
+    const { handlebars } = options;
+
+    this.handlebars = handlebars;
+    this.template   = readFileSync(templatePath, `utf8`);
+    this.compile    = this.handlebars.compile(this.template);
+    this.handlebars.registerPartial({ reference: this.template });
+    this.handlebars.registerHelper(helpers);
+
   }
 
 }
--- test/node.test.js
+++ test/node.test.js
@@ -1,17 +1,29 @@
-const handlebars = require(`handlebars`);
+const Handlebars = require(`handlebars`);
 const LingRef    = require(`../src/index.js`);
 
 describe(`LingRef`, () => {
 
   it(`registers the partial with Handlebars`, () => {
     new LingRef; // eslint-disable-line no-new
-    expect(handlebars.partials.reference).toBeDefined();
+    const compile = Handlebars.compile(`{{> reference}}`);
+    const html    = compile({});
+    expect(typeof html).toBe(`string`);
   });
 
-  it(`compiles a reference to HTML`, () => {
+  it(`.compile()`, () => {
     const lingRef = new LingRef;
     const html    = lingRef.compile({});
     expect(typeof html).toBe(`string`);
+  });
+
+  describe(`options`, () => {
+
+    it(`handlebars`, () => {
+      const hbs = Handlebars.create();
+      new LingRef({ handlebars: hbs }); // eslint-disable-line no-new
+      expect(hbs.partials.reference).toBeDefined();
+    });
+
   });
 
 });
