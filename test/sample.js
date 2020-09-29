9829ee0d3ae2b6616c7176dc8fe6c0b557844b94
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-29 16:01:01 -0500

    support generics

--- test/sample.js
+++ test/sample.js
@@ -6,9 +6,7 @@ const { readJSON } = fs;
 void async function sample() {
 
   const documents = await readJSON(`test/Algonquian.json`);
-  const webpages = documents.filter(doc => doc.type === `web_page`);
+  const generics = documents.filter(doc => doc.type === `unspecified`);
+  console.log(generics);
 
 }();
-
-const d = new Date;
-console.log(new Intl.DateTimeFormat().format(d));
