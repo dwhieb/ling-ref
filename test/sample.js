b2213815444155e1cb8364a0c61e820352b3220d
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-29 15:49:10 -0500

    NEW: support web pages (#112)
    closes #112

--- test/sample.js
+++ test/sample.js
@@ -6,7 +6,9 @@ const { readJSON } = fs;
 void async function sample() {
 
   const documents = await readJSON(`test/Algonquian.json`);
-  const reports = documents.filter(doc => doc.type === `report`);
-  console.log(reports);
+  const webpages = documents.filter(doc => doc.type === `web_page`);
 
 }();
+
+const d = new Date;
+console.log(new Intl.DateTimeFormat().format(d));
